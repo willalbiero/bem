608d1303985df71b9ab9cda0af8133457f0fe532
William Albiero <albiero.william@gmail.com>
2019-08-31 04:27:39 -0300

    first commit

---
+++ database/migrations/2019_08_31_065915_create_kits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kits', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->decimal('preco', 8, 2);
+            $table->string('tipo_licenca');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kits');
+    }
+}
